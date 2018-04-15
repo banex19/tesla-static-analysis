6ae38aa34153f9d1d3afa131f4abbb6f7546acfa
Daniele Vettorel <vettoreldaniele@gmail.com>
2018-04-15 23:38:16 +0200

    Fixes to types

--- tesla/instrumenter/ThinTeslaTypes.cpp
+++ tesla/instrumenter/ThinTeslaTypes.cpp
@@ -1,6 +1,14 @@
 #include "ThinTeslaTypes.h"
+#include "llvm/Support/raw_ostream.h"
 
 bool TeslaTypes::populated = false;
+StructType* TeslaTypes::AutomatonFlagsTy = nullptr;
+StructType* TeslaTypes::AutomatonStateTy = nullptr;
+StructType* TeslaTypes::AutomatonTy = nullptr;
+
+StructType* TeslaTypes::EventFlagsTy = nullptr;
+StructType* TeslaTypes::EventStateTy = nullptr;
+StructType* TeslaTypes::EventTy = nullptr;
 
 StructType* TeslaTypes::GetStructType(StringRef name, ArrayRef<Type*> fields, Module& M)
 {
@@ -25,8 +33,6 @@ void TeslaTypes::PopulateEventTy(Module& M)
     LLVMContext& C = M.getContext();
 
     Type* VoidTy = Type::getVoidTy(C);
-    PointerType* VoidPtrTy = PointerType::getUnqual(VoidTy);
-    PointerType* VoidPtrPtrTy = PointerType::getUnqual(VoidPtrTy);
     IntegerType* CharTy = IntegerType::getInt8Ty(C);
     PointerType* CharPtrTy = PointerType::getUnqual(CharTy);
     PointerType* CharPtrPtrTy = PointerType::getUnqual(CharPtrTy);
@@ -34,7 +40,9 @@ void TeslaTypes::PopulateEventTy(Module& M)
     IntegerType* SizeTTy = IntegerType::get(C, sizeof(size_t) * 8);
     IntegerType* Int8Ty = IntegerType::getInt8Ty(C);
     PointerType* Int8PtrTy = PointerType::getUnqual(Int8Ty);
-    //  IntegerType* IntPtrTy = DataLayout(&M).getIntPtrType(C);
+    PointerType* VoidPtrTy = Int8PtrTy;
+    PointerType* VoidPtrPtrTy = PointerType::getUnqual(Int8PtrTy);
+    //IntegerType* IntPtrTy = DataLayout(&M).getIntPtrType(C);
 
     EventFlagsTy = GetStructType("TeslaEventFlags", {Int8Ty}, M);
     EventStateTy = GetStructType("TeslaEventState", {VoidPtrTy, Int8PtrTy}, M);
@@ -46,8 +54,6 @@ void TeslaTypes::PopulateAutomatonTy(Module& M)
     LLVMContext& C = M.getContext();
 
     Type* VoidTy = Type::getVoidTy(C);
-    PointerType* VoidPtrTy = PointerType::getUnqual(VoidTy);
-    PointerType* VoidPtrPtrTy = PointerType::getUnqual(VoidPtrTy);
     IntegerType* CharTy = IntegerType::getInt8Ty(C);
     PointerType* CharPtrTy = PointerType::getUnqual(CharTy);
     PointerType* CharPtrPtrTy = PointerType::getUnqual(CharPtrTy);
@@ -56,6 +62,8 @@ void TeslaTypes::PopulateAutomatonTy(Module& M)
     IntegerType* Int8Ty = IntegerType::getInt8Ty(C);
     IntegerType* BoolTy = Int8Ty;
     PointerType* Int8PtrTy = PointerType::getUnqual(Int8Ty);
+    PointerType* VoidPtrTy = Int8PtrTy;
+    PointerType* VoidPtrPtrTy = PointerType::getUnqual(Int8PtrTy);
 
     PointerType* EventPtrTy = PointerType::getUnqual(EventTy);
     PointerType* EventPtrPtrTy = PointerType::getUnqual(EventPtrTy);
