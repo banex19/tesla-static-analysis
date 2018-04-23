a7fe1707e215b8b0fae48e8b242d77d4208ce521
Daniele Vettorel <vettoreldaniele@gmail.com>
2018-04-23 17:31:05 +0000

    Inlining of bit operations

--- libtesla/c_thintesla/TeslaUtils.c
+++ libtesla/c_thintesla/TeslaUtils.c
@@ -7,19 +7,9 @@ uint64_t FirstZero(uint64_t x)
     return RightmostOne(~(x));
 }
 
-uint64_t LeftmostOne(uint64_t x)
-{
-    return 63 - __builtin_clzll(x);
-}
-
 uint64_t RightmostOne(uint64_t x)
 {
     return __builtin_ctzll(x);
-}
-
-bool IsPowerOfTwo(size_t x)
-{
-    return (x & (x - 1)) == 0;
 }
 
 uint64_t SetBit(uint64_t x, uint64_t bit)
@@ -34,9 +24,19 @@ uint64_t ClearBit(uint64_t x, uint64_t bit)
     return x;
 }
 
+/*
 uint64_t MaskBetweenExclUpper(uint64_t x, uint64_t y)
 {
     return x - y;
 }
+
+uint64_t LeftmostOne(uint64_t x)
+{
+    return 63 - __builtin_clzll(x);
+}
+bool IsPowerOfTwo(size_t x)
+{
+    return (x & (x - 1)) == 0;
+}*/
 
 _Static_assert(sizeof(uint64_t) >= sizeof(TeslaTemporalTag), "Temporal tag type too large");
--- libtesla/c_thintesla/TeslaUtils.h
+++ libtesla/c_thintesla/TeslaUtils.h
@@ -4,11 +4,18 @@
 EXTERN_C
 
 uint64_t FirstZero(uint64_t x);
-uint64_t LeftmostOne(uint64_t x);
+
 uint64_t RightmostOne(uint64_t x);
-bool IsPowerOfTwo(uint64_t x);
+
 uint64_t SetBit(uint64_t x, uint64_t bit);
 uint64_t ClearBit(uint64_t x, uint64_t bit);
+/*
 uint64_t MaskBetweenExclUpper(uint64_t x, uint64_t y);
+bool IsPowerOfTwo(uint64_t x);
+uint64_t LeftmostOne(uint64_t x); */
+
+#define LeftmostOne(VAR) (63 - __builtin_clzll(VAR))
+#define IsPowerOfTwo(VAR) ((VAR & (VAR - 1)) == 0)
+#define MaskBetweenExclUpper(VARX, VARY) (VARX - VARY)
 
 EXTERN_C_END
